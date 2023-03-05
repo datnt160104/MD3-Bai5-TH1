5a82d8a63efecce866c0e3d7a184306f03e9a483
Đạt <dat16010425@gmail.com>
2023-03-05 22:00:31 +0700

    th1

---
+++ src/rikkei/academy/Student.java
@@ -0,0 +1,18 @@
+package rikkei.academy;
+
+public class Student {
+        private int rollNo;
+        private String name;
+        private static String college = "BBIT";
+
+        public Student(int rollNo, String name) {
+            this.rollNo = rollNo;
+            this.name = name;
+        }
+        static void change () {
+            college = "Oxford";
+        }
+        void display () {
+            System.out.println(rollNo + " " + name + " " + college);
+        }
+}
